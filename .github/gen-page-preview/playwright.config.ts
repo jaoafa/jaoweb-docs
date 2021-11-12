4b4338521b0e7b8a2a45eb4728841d97f73c4dec
Tomachi [ICHIGO] <lego@aol.jp>
2021-11-12 15:24:46 +0900

    fix: yarn startをするディレクトリをGITHUB_WORKSPACEへ変更

--- .github/gen-page-preview/playwright.config.ts
+++ .github/gen-page-preview/playwright.config.ts
@@ -1,8 +1,9 @@
-import { PlaywrightTestConfig } from '@playwright/test'
+import { PlaywrightTestConfig } from "@playwright/test";
 const config: PlaywrightTestConfig = {
   webServer: {
-    command: 'yarn start',
+    command: "yarn start",
+    cwd: process.env.GITHUB_WORKSPACE,
     port: 3000,
   },
-}
-export default config
+};
+export default config;
